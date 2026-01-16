c07502d15e7dcc0b2a139437c26f282e4bf5d6d2
srdlzsy <srdlzsyy@gmail.com>
2026-01-16 09:33:58 +0300

    Refine brand favicon and apple icon

--- scripts/generate-icons.ps1
+++ scripts/generate-icons.ps1
@@ -1,34 +1,46 @@
 Add-Type -AssemblyName System.Drawing
 
-# 64x64 favicon with 'M'
-$bmp = New-Object Drawing.Bitmap 64,64
-$g = [Drawing.Graphics]::FromImage($bmp)
-$g.SmoothingMode = 'AntiAlias'
-$g.Clear([Drawing.Color]::FromArgb(255,37,99,235))
-$f = New-Object Drawing.Font('Segoe UI Black',34)
-$sf = New-Object Drawing.StringFormat
-$sf.Alignment='Center'
-$sf.LineAlignment='Center'
-$g.DrawString('M',$f,[Drawing.Brushes]::White,(New-Object Drawing.RectangleF(0,0,64,64)),$sf)
-$bmp.Save('public/favicon.png',[Drawing.Imaging.ImageFormat]::Png)
+function New-BrandIcon {
+    param(
+        [int]$Size,
+        [string]$OutPng,
+        [string]$Text
+    )
+
+    $bmp = New-Object Drawing.Bitmap $Size, $Size
+    $g = [Drawing.Graphics]::FromImage($bmp)
+    $g.SmoothingMode = 'AntiAlias'
+
+    $rect = New-Object Drawing.Rectangle 0, 0, $Size, $Size
+
+    # Gradient background (navy → blue)
+    $bg = New-Object Drawing.Drawing2D.LinearGradientBrush $rect, ([Drawing.Color]::FromArgb(255,15,23,42)), ([Drawing.Color]::FromArgb(255,37,99,235)), 60
+    $g.FillRectangle($bg, $rect)
+
+    # Soft border for depth
+    $border = New-Object Drawing.Pen ([Drawing.Color]::FromArgb(80,255,255,255)), 2
+    $g.DrawRectangle($border, 1, 1, $Size - 3, $Size - 3)
+
+    # Centered logotype
+    $fontSize = if ($Size -le 64) { 28 } else { 78 }
+    $font = New-Object Drawing.Font('Segoe UI Black', $fontSize, [Drawing.FontStyle]::Bold)
+    $sf = New-Object Drawing.StringFormat
+    $sf.Alignment = 'Center'
+    $sf.LineAlignment = 'Center'
+    $g.DrawString($Text, $font, [Drawing.Brushes]::White, (New-Object Drawing.RectangleF(0,0,$Size,$Size)), $sf)
+
+    $g.Dispose()
+    $bmp.Save($OutPng, [Drawing.Imaging.ImageFormat]::Png)
+    return $bmp
+}
+
+# Brand letter: use "MS" to match MSYazılımDuo
+$small = New-BrandIcon -Size 64 -OutPng 'public/favicon.png' -Text 'MS'
 $fs = [IO.File]::Create('public/favicon.ico')
-$icon = [System.Drawing.Icon]::FromHandle($bmp.GetHicon())
+$icon = [System.Drawing.Icon]::FromHandle($small.GetHicon())
 $icon.Save($fs)
 $fs.Close()
-$g.Dispose()
-$bmp.Dispose()
+$small.Dispose()
 
-# 180x180 apple touch icon
-$bmp2 = New-Object Drawing.Bitmap 180,180
-$g2 = [Drawing.Graphics]::FromImage($bmp2)
-$g2.SmoothingMode='AntiAlias'
-$g2.Clear([Drawing.Color]::FromArgb(255,12,18,40))
-$brush2 = New-Object Drawing.Drawing2D.LinearGradientBrush((New-Object Drawing.Rectangle(0,0,180,180)),[Drawing.Color]::FromArgb(255,37,99,235),[Drawing.Color]::FromArgb(255,59,130,246),60)
-$g2.FillRectangle($brush2,0,0,180,180)
-$pen = [Drawing.Pen]::new([Drawing.Color]::FromArgb(40,255,255,255),4)
-$g2.DrawEllipse($pen,10,10,160,160)
-$f2 = New-Object Drawing.Font('Segoe UI Black',78)
-$g2.DrawString('M',$f2,[Drawing.Brushes]::White,(New-Object Drawing.RectangleF(0,0,180,180)),$sf)
-$bmp2.Save('public/apple-icon.png',[Drawing.Imaging.ImageFormat]::Png)
-$g2.Dispose()
-$bmp2.Dispose()
+$apple = New-BrandIcon -Size 180 -OutPng 'public/apple-icon.png' -Text 'MS'
+$apple.Dispose()
